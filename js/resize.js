43f2bf89a565de6e0f01552efa02f62bc2e1e414
zelkor <zelkor.mail@gmail.com>
2017-06-06 17:17:43 +0300

    Вернул всё нафиг

--- js/resize.js
+++ js/resize.js
@@ -1,13 +1,7 @@
 function resizeMain() {
-	var user = detect.parse(navigator.userAgent);
-	if (user.browser.family == "Firefox") {
-		var add = 20;
-	}
-	else {
-		var add = 0;
-	}
-	if (document.body.clientHeight < (640 + add)) {
-		document.getElementById("main").style.height = toString(450 + add) + "px";
+	
+	if (document.body.clientHeight < (640)) {
+		document.getElementById("main").style.height = "450px";
 	}
 }
 
