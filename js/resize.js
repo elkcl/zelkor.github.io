32d1f23aabded74521b4fcb0214adeae98e7fdc6
zelkor <zelkor.mail@gmail.com>
2017-06-06 17:06:36 +0300

    Совсем штрихи

--- js/resize.js
+++ js/resize.js
@@ -1,6 +1,13 @@
 function resizeMain() {
-	if (document.body.clientHeight < 660) {
-		document.getElementById("main").style.height = "470px";
+	var user = detect.parse(navigator.userAgent);
+	if (user.browser.family == "Firefox") {
+		var add = 20;
+	}
+	else {
+		var add = 0;
+	}
+	if (document.body.clientHeight < (640 + add)) {
+		document.getElementById("main").style.height = toString(450 + add) + "px";
 	}
 }
 
