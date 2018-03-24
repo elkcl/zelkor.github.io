82dcc37a11fd049b12b74ac0c6204391c978b54f
elkcl <elkcl@zelkor.ml>
2018-03-24 18:41:25 +0300

    Час Земли 2018

--- _site/js/resize.js
+++
@@ -1,9 +0,0 @@
-function resizeMain() {
-  
-  if (document.body.clientHeight < (640)) {
-    document.getElementById("main").style.height = "420px";
-  }
-}
-
-window.onload = resizeMain;
-window.onresize = resizeMain;
