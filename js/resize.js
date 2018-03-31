c380756153a03a964a1c87c7345397e65444d06c
zelkor <zelkor.mail@gmail.com>
2018-03-31 22:01:10 +0300

    Update resize.js

--- js/resize.js
+++ js/resize.js
@@ -1,9 +1,12 @@
 function resizeMain() {
-  
-  if (document.body.clientHeight < (640)) {
-    document.getElementById("main").style.height = "420px";
+  if (document.body.clientWidth > (800)) {
+    new ResizeSensor(document.getElementById("main"), function () {
+      document.getElementById("main").style.height = "";
+      if (document.body.clientHeight < (640)) {
+        document.getElementById("main").style.height = "420px";
+      }
+    });
   }
 }
 
 window.onload = resizeMain;
-window.onresize = resizeMain;
